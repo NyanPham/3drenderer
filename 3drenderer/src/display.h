54e77c787089223269e0f7c11b2f958b8b999d08
Nyan <phamthanhnhanussh@gmail.com>
2026-04-01 18:54:40 +0700

    Refactored main.c for display.h and display.c

---
+++ 3drenderer/src/display.h
@@ -0,0 +1,21 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+#include <SDL2/SDL.h>
+
+extern SDL_Window* g_window;
+extern SDL_Renderer* g_renderer;
+extern uint32_t* g_color_buffer;
+extern SDL_Texture * g_color_buffer_texture;
+extern int window_width;
+extern int window_height;
+
+int initialize_window(void);
+void draw_grid(void);
+void draw_rect(int x, int y, int w, int h, uint32_t color);
+void render_color_buffer(void);
+void clear_color_buffer(uint32_t color);
+void destroy_window(void);
+
+#endif
--- 3drenderer/src/main.c
+++ 3drenderer/src/main.c
@@ -1,58 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <SDL2/SDL.h>
+#include "display.h"
 
 uint8_t is_running = 0;
-SDL_Window* g_window = NULL;
-SDL_Renderer* g_renderer = NULL;
 
-uint32_t* g_color_buffer = NULL;
-SDL_Texture * g_color_buffer_texture = NULL;
-
-int window_width = 800;
-int window_height = 600;
-
-int initialize_window(void) {
-    
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL.\n");
-        return -1;
-    }
-
-    // Use SDL to query what the fullscreen max. width and height are
-    SDL_DisplayMode display_mode;
-    SDL_GetCurrentDisplayMode(0, &display_mode);
-    window_width = display_mode.w;
-    window_height = display_mode.h;
-
-    // Create an SDL Window
-    g_window = SDL_CreateWindow(
-        NULL,
-        SDL_WINDOWPOS_CENTERED, // posX
-        SDL_WINDOWPOS_CENTERED, // poxY
-        window_width,           // width
-        window_height,          // height
-        SDL_WINDOW_BORDERLESS
-    );
-
-    if (!g_window) {
-        fprintf(stderr, "Error creating the SDL window.\n");
-        return -1;
-    }
-    
-    // Create an SDL renderer
-    g_renderer = SDL_CreateRenderer(g_window, -1, 0);
-
-    if (!g_renderer) {
-        fprintf(stderr, "Error creating the SDL Renderer.\n");
-        return -1;
-    }
-
-    SDL_SetWindowFullscreen(g_window, SDL_WINDOW_FULLSCREEN);
-
-    return 0;
-    
-}
 
 void setup(void) {
     // allocate required mem in bytes to hold the color buffer
@@ -88,42 +40,6 @@ void process_input(void) {
 
 void update(void) {}
 
-void draw_grid(void) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            if ((x % 10) == 0 || (y % 10) == 0) {
-                g_color_buffer[(window_width * y) + x] = 0xFF333333;
-            }
-        }
-    }
-}
-
-void draw_rect(int x, int y, int w, int h, uint32_t color) {
-    for (int i = 0; i < w; i++) {
-        for (int j = 0; j < h; j++) {
-            g_color_buffer[(j + y) * window_width + (i + x)] = color;
-        }
-    }
-}
-
-void clear_color_buffer(uint32_t color) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            g_color_buffer[(window_width * y) + x] = color;
-        }
-    }
-}
-
-void render_color_buffer(void) {
-    SDL_UpdateTexture(
-        g_color_buffer_texture,
-        NULL,
-        g_color_buffer,
-        (int)(window_width * sizeof(uint32_t))
-    );
-    SDL_RenderCopy(g_renderer, g_color_buffer_texture, NULL, NULL); 
-}
-
 void render(void) {
     SDL_SetRenderDrawColor(g_renderer, 255, 0, 0, 255);
     SDL_RenderClear(g_renderer);
@@ -134,13 +50,6 @@ void render(void) {
     render_color_buffer();
     clear_color_buffer(0xFF000000);
     SDL_RenderPresent(g_renderer);
-}
-
-void destroy_window(void) {
-    free(g_color_buffer);
-    SDL_DestroyRenderer(g_renderer);
-    SDL_DestroyWindow(g_window);
-    SDL_Quit();
 }
 
 int main(void) {
